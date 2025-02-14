a9f54ec6dc158a5aee6aa684f3678874d14cfb33
Ritabrata <ritabratamandal03@gmail.com>
2025-02-14 14:39:48 +0530

    Create velocity_cal.m

---
+++ odometry_analysis/velocity_cal.m
@@ -0,0 +1,21 @@
+function [v, w, prevWheelPosGazebo, prevTimeGazebo] = velocity_cal(jointStateSubGazebo, prevWheelPosGazebo, prevTimeGazebo)
+    wheelBase = 0.16;    % meters
+    jointStateMsgGazebo = receive(jointStateSubGazebo, 1);
+    wheelPosGazebo = jointStateMsgGazebo.Position(1:2);
+    currentTimeGazebo = jointStateMsgGazebo.Header.Stamp.Sec + ...
+                        jointStateMsgGazebo.Header.Stamp.Nsec * 1e-9;
+    
+    v = 0;
+    w = 0;
+    
+    if prevTimeGazebo > 0
+        dt = currentTimeGazebo - prevTimeGazebo;
+        dvl = (wheelPosGazebo(2) - prevWheelPosGazebo(2)) / dt;
+        dvr = (wheelPosGazebo(1) - prevWheelPosGazebo(1)) / dt;
+        v = (dvl + dvr) / 2;
+        w = (dvr - dvl) / wheelBase;
+    end
+    
+    prevWheelPosGazebo = wheelPosGazebo;
+    prevTimeGazebo = currentTimeGazebo;
+end
